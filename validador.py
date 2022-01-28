0c432e3962e7047a31d97cc1136e5e3cda70f91a
FernandoCamousseigt <82519981+FernandoCamousseigt@users.noreply.github.com>
2022-01-28 12:01:56 -0300

    Add files via upload

---
+++ validador.py
@@ -0,0 +1,23 @@
+
+def validate(opciones, eleccion):
+    # Definir validación de eleccion
+    ##########################################################################
+    #pass
+    while eleccion not in opciones:  #mientras la eleccion no este en la lista de opciones
+        eleccion = input('Opcion inválida. Ingrese una de las opciones válidas: ')
+
+    return eleccion  #si se ingresa una opcion valida se retorna el valor de la eleccion válida
+
+    ##########################################################################
+    return eleccion
+
+
+if __name__ == '__main__':
+    
+    eleccion = input('Ingresa una Opción: ').lower()
+    # letras = ['a','b','c','d'] # pueden probar con letras también para verificar su funcionamiento.
+    numeros = ['0','1']
+    # Si se ingresan valores no validos a eleccion debe seguir preguntando
+    validate(numeros, eleccion)
+    
+    
